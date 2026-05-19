30e6184e5afff1578b379d609d75b1fe8c833171
Ferran <ferran.docencia@gmail.com>
2026-05-19 18:34:47 +0200

    Javascript

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
+// Obtener el input de fecha de entrada y salida
+const fechaEntrada = document.getElementById('fechaEntrada')
+const fechaSalida = document.getElementById('fechaSalida')
+
 // Obtener la fecha de hoy
 const today = new Date()
 
 const todayYear = today.getFullYear()
 const todayMonth = String(today.getMonth() + 1)
 const todayDay = String(today.getDate())
-// console.log(todayDay, todayMonth, todayYear);
-// console.log(typeof todayMonth);
 
-function corregirFecha(dato) {
-    if (dato.length == 1) {
-        dato = "0" + dato
-    }
-    return dato
-}
-
-const fechaEntrada = document.getElementById('fechaEntrada')
+// Definir la fecha de hoy
 const fechaHoy = `${todayYear}-${corregirFecha(todayMonth) }-${corregirFecha(todayDay)}`
 fechaEntrada.value = fechaHoy
 fechaEntrada.setAttribute('min', fechaHoy)
 
 // Obtener la fecha de mañana
+let tomorrow = today.getTime()+(1000*60*60*24)
+tomorrow = new Date(tomorrow)
+const tomorrowDay = tomorrow.getDate()
+const tomorrowMonth = String(tomorrow.getMonth() + 1)
+const tomorrowYear = String(tomorrow.getFullYear())
 
+const fechaTomorrow = `${tomorrowYear}-${corregirFecha(tomorrowMonth) }-${corregirFecha(tomorrowDay)}`
+// console.log(fechaTomorrow);
+fechaSalida.value = fechaTomorrow
+fechaSalida.setAttribute('min', fechaSalida)
 
 // Obtener el formulario
 const formReserva = document.forms["formReserva"];
@@ -33,3 +37,11 @@ formReserva.addEventListener("submit", (e) => {
   let fechaEntrada = formReserva["fechaEntrada"].value;
   let fechaSalida = formReserva["fechaSalida"].value;
 });
+
+
+function corregirFecha(dato) {
+    if (dato.length == 1) {
+        dato = "0" + dato
+    }
+    return dato
+}
